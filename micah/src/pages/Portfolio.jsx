3a083f1677c3a1201caa48b0d23e62d83f5a2690
Micah Francis <micah.francis69@googlemail.com>
2026-03-25 20:34:33 +0000

    setting up a new carousel

--- micah/src/pages/Portfolio.jsx
+++ micah/src/pages/Portfolio.jsx
@@ -1,78 +1,55 @@
-import React, {useState} from 'react'
-import {motion} from 'framer-motion';
+import React, {useEffect, useRef, useState} from 'react'
+import {motion, useMotionValue} from 'framer-motion';
 import styles from "../styles/Portfolio.module.css";
 import LanguageBadge from '../components/LanguageBadge';
 import {portfolioData} from '../data/portfolioData'
 
 const Portfolio = () => {
+  const [dragging, setDragging] = useState(false);
     const [index, setIndex] = useState(0)
-    const port = portfolioData[index];
 
-    const handleNext = () => {
-        setIndex((prev) => (prev + 1) % portfolioData.length)
+    const dragX = useMotionValue(0);
+    const DRAG_BUFFER = 50;
+
+    const onDragStart = () => {
+      setDragging(true)
+      console.log(dragX.get())
     }
 
-    const handlePrev = () => {
-        setIndex((prev) => (prev - 1 + portfolioData.length) % portfolioData.length)
+    const onDragEnd = () => {
+      setDragging(false)
+      const x = dragX.get()
+      if (x <= -DRAG_BUFFER && index < extended.length - 1) {
+        setIndex(prev => prev + 1)
+      } else if (x >= DRAG_BUFFER && index > 0) {
+        setIndex(prev => prev - 1)
+      }
     }
 
-    const imageVariants = {
-        center: { x: "300px", scale: 1, zIndex: 5 },
-        left1: { x: "150px", scale: 0.7, zIndex: 3 },
-        left: { x: "0px", scale: 0.5, zIndex: 1 },
-        right1: { x: "450px", scale: 0.7, zIndex: 3 },
-        right: { x: "600px", scale: 0.5, zIndex: 1 },
-
-        hidden: { opacity: 0, scale: 0.3, zIndex: 0 }
-    };
-
-
-    const getPosition = (i) => {
-        const diff = (i - index + portfolioData.length) % portfolioData.length;
-
-        if (diff === 0) return "center";
-        if (diff === 1) return "right1";
-        if (diff === 2) return "right";
-        if (diff === portfolioData.length - 1) return "left1";
-        if (diff === portfolioData.length - 2) return "left";
-
-        return "hidden";
-    };
-
+    const extended = [...portfolioData];
 
   return (
     <div>
         <section style={{height: "25vh", display: "flex", alignItems: "center"}}>
             <h1>Portfolio</h1>
         </section>
-        <section style={{display: "flex", flexDirection: "column", alignItems: "center", height: "100vh"}}>
-            <div style={{position: "relative", height:"500px", width: "900px", overflow: "visible"}}>
-            {portfolioData.map((port, i) => (
-                <motion.div 
-                key={port.id}
-                animate={getPosition(i)}
-                variants={imageVariants}
-                transition={{duration: 0.5}}
-                 style={{position: "absolute",
-                  top: 0, left: 0, 
-                   width: "300px", 
-                   height: "400px", 
-                   borderRadius: "15px", 
-                   border: "1px solid black", 
-                   padding: "1rem", 
-                   backgroundColor: "white"}}>
-                    <h3 >{port.name}</h3>
-                    <img style={{ width: "100%"}} src={port.image} alt="" />
-                    <h5 style={{marginBottom: 0}}>Description</h5>
-                    <p style={{fontSize: "0.75rem"}}>{port.description}</p>
-                    {port.languages.map(lan => <LanguageBadge key={lan} lan={lan} />)}
-                </motion.div>
-            ))}
-            </div>
-            <div style={{display: "flex"}}>
-                <button onClick={handlePrev}>Prev</button>
-                <button onClick={handleNext}>Next</button>
-            </div>
+        <section style={{postion: "relative", height: "100vh", backgroundColor: "lightgray", padding: "2rem 0", overflow: "hidden"}}>
+          <motion.div 
+          drag="x" 
+          dragConstraints={{left: 0, right: 0}}
+          style={{x: dragX, display: "flex", alignItems: 'center', cursor: 'grab', height: "100%"}}
+          animate={{
+            translateX: `-${index * 100}%`
+          }}
+          whileTap={{cursor: "grabbing"}}
+          onDragStart={onDragStart}
+          onDragEnd={onDragEnd}
+          >
+          {extended.map((ext, i) => (
+            <div key={i} style={{height: "100%", width: "100%", flexShrink: 0, backgroundColor: "lightblue", border: "2px solid black"}}>{ext.name}</div>
+          ))}
+          
+          </motion.div>
         </section>
     </div>
   )
