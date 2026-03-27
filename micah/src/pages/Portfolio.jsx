1f5f2c613472c9dd8b9a17a25a3e027a4caed4d7
Micah Francis <micah.francis69@googlemail.com>
2026-03-27 08:23:59 +0000

    Fixed the drifting bug on the carousel

--- micah/src/pages/Portfolio.jsx
+++ micah/src/pages/Portfolio.jsx
@@ -5,88 +5,99 @@ import LanguageBadge from '../components/LanguageBadge';
 import {portfolioData} from '../data/portfolioData'
 
 const Portfolio = () => {
-  const [dragging, setDragging] = useState(false);
+  
     const [index, setIndex] = useState(0)
 
-    const dragX = useMotionValue(0);
-
-    useEffect(() => {
-      const intervalRef = setInterval(() => {
-        
-      }, 1000);
-      return () => clearInterval(intervalRef)
-    }, [])
+    const ONE_SECOND = 1000;
+    const AUTO_DELAY = ONE_SECOND * 10;
     const DRAG_BUFFER = 50;
-  const SPRING_OPTIONS = {
-    type: 'spring',
-    mass: 3,
-    stiffness: 400,
-    damping: 30
-  }
-
-    const onDragStart = () => {
-      setDragging(true)
-      console.log(dragX.get())
+    const SPRING_OPTIONS = {
+      type: 'spring',
+      mass: 3,
+      stiffness: 400,
+      damping: 50
     }
+    
+  const dragX = useMotionValue(0);
+  
+  useEffect(() => {
+    const intervalRef = setInterval(() => {
+      const x = dragX.get();
 
-    const onDragEnd = () => {
-      setDragging(false)
-      const x = dragX.get()
-      if (x <= -DRAG_BUFFER && index < extended.length - 1) {
-        setIndex(prev => prev + 1)
-      } else if (x >= DRAG_BUFFER && index > 0) {
-        setIndex(prev => prev - 1)
+      if (x === 0) {
+        setIndex((prev) => {
+          if(prev === portfolioData.length - 1) {
+            return 0
+          }
+          return prev + 1
+        });
       }
-    }
+    }, AUTO_DELAY);
 
-    const extended = [...portfolioData];
+    return () => clearInterval(intervalRef)
+  }, []);
+
+
+  const onDragEnd = () => {
+    const x = dragX.get();
+    if (x <= -DRAG_BUFFER && index < portfolioData.length - 1) {
+      setIndex(prev => prev + 1)
+    } else if (x >= DRAG_BUFFER && index > 0) {
+      setIndex(prev => prev - 1)
+    }
+    dragX.set(0);
+  }
 
   return (
     <div>
-        <section style={{height: "25vh", display: "flex", alignItems: "center"}}>
-            <h1>Portfolio</h1>
-        </section>
-        <section style={{postion: "relative", height: "100vh", backgroundColor: "lightgray", padding: "2rem 0", overflow: "hidden"}}>
-          <motion.div 
-          drag="x" 
-          dragConstraints={{left: 0, right: 0}}
-          style={{x: dragX, display: "flex", alignItems: 'center', cursor: 'grab', height: "100%"}}
-          animate={{
-            translateX: `-${index * 100}%`
-          }}
+      <section style={{height: "25vh", display: "flex", alignItems: "center"}}>
+          <h1>Portfolio</h1>
+      </section>
+      <section style={{ position: "relative", height: "100vh", backgroundColor: "lightgray", overflow: "hidden" }}>
+        <motion.div
+          drag="x"
+          dragConstraints={{ left: 0, right: 0 }}
+          animate={{translateX: `-${index * 100}%`}}
           transition={SPRING_OPTIONS}
-          whileTap={{cursor: "grabbing"}}
-          onDragStart={onDragStart}
+          whileTap={{ cursor: "grabbing" }}
           onDragEnd={onDragEnd}
-          >
-          {extended.map((ext, i) => (
-            <motion.div 
-            key={i} 
-            style={{
-              height: "100%", width: "100%", 
-              flexShrink: 0, backgroundColor: "lightblue",
-              border: "2px solid black",
-            }}
-            animate={{scale: index === i ? 0.95 : 0.85}}
-            transition={SPRING_OPTIONS}
-            >{ext.name}</motion.div>
+          style={{x: dragX, display: "flex", alignItems: 'center', cursor: 'grab', height: "90%" }}
+        >
+          {portfolioData.map((ext, i) => (
+            <motion.div
+              key={i}
+              style={{
+                height: "100%", width: "100%",
+                flexShrink: 0, backgroundColor: "lightblue",
+                boxSizing: 'border-box',
+                border: "2px solid black", 
+                borderRadius: "10px",
+                perspective: '1000px',
+                overflow: 'hidden'
+              }}
+              animate={{ scale: index === i ? 0.95 : 0.85 }}
+              transition={SPRING_OPTIONS}
+            >
+              <h1>{ext.name}</h1>
+              <img style={{objectFit: 'cover', height: '100%', width: '100%', transform: 'rotateX(45deg) rotateY(-30deg)', transformStyle: 'preserve-3d'}} src={ext.image} alt="" />
+            </motion.div>
           ))}
-          
-          </motion.div>
-          <Dots index={index} setIndex={setIndex} />
-        </section>
+
+        </motion.div>
+        <Dots index={index} setIndex={setIndex} />
+      </section>
     </div>
   )
 }
 
 const Dots = ({ index, setIndex }) => {
   return (
-    <div style={{display: 'flex', justifyContent: 'center', marginTop: '16px', gap: 2}}>
+    <div style={{display: 'flex', justifyContent: 'center', marginTop: '4px', gap: 4}}>
       {portfolioData.map((_, i) => (
         <button 
         key={i} 
         onClick={() => setIndex(i)} 
-        style={{height: '12px', width: '12px', borderRadius: '1000px', border: 'none', padding: 0}} 
+        style={{height: '12px', width: '12px', borderRadius: '1000px', border: 'none', padding: 0, cursor: 'pointer'}} 
         className={`${styles.dots} ${i === index ? styles.dotsActive : ""}`}
         />
       ))}
