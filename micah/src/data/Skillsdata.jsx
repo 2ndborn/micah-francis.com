2bf4c9332463c7338bf3a9cfc419ae4c2f2a2d59
Micah Francis <micah.francis69@googlemail.com>
2026-04-25 13:55:31 +0100

    Added icons to the skills data

--- micah/src/data/Skillsdata.jsx
+++ micah/src/data/Skillsdata.jsx
@@ -14,7 +14,28 @@ import { SiDjango } from "react-icons/si";
 import { BiLogoPostgresql } from "react-icons/bi";
 import { SiMongodb } from "react-icons/si";
 
+import { GiLighthouse } from "react-icons/gi";
+import { LuSpeech } from "react-icons/lu";
+import { FaPeopleGroup } from "react-icons/fa6";
+import { FaPuzzlePiece } from "react-icons/fa6";
+import { FaExchangeAlt } from "react-icons/fa";
+import { FaRegLightbulb } from "react-icons/fa";
+import { IoMdTime } from "react-icons/io";
+import { PiHeadCircuitFill } from "react-icons/pi";
+import { GoChecklist } from "react-icons/go";
+import { LuHeartHandshake } from "react-icons/lu";
+
 export const Skills = {
+    Leadership: GiLighthouse,
+    Communication: LuSpeech,
+    Teamwork: FaPeopleGroup,
+    ProblemSolving: FaPuzzlePiece,
+    Adaptable: FaExchangeAlt,
+    Innovative: FaRegLightbulb,
+    TimeManagement: IoMdTime,
+    CriticalThinking: PiHeadCircuitFill,
+    WorkEthic: GoChecklist,
+    EmotionalInteligence: LuHeartHandshake,
     HTML: FaHtml5,
     CSS: FaCss3Alt,
     JavaScript: FaJs,
