ffbba4e76e6435fb1a3737c001a62911f2af5327
Micah Francis <micah.francis69@googlemail.com>
2026-04-26 11:33:32 +0100

    Added links to the footer buttons

--- micah/src/components/Footer.jsx
+++ micah/src/components/Footer.jsx
@@ -21,10 +21,40 @@ const Footer = () => {
         className={styles.footerContainer}>
             <h2 style={{ textAlign: "center" }}>Contact</h2>
             <div className={styles.upperFooter}>
-                <button>WhatsApp<i className="fa-brands fa-whatsapp"></i></button>
-                <button>Email<i className="fa-regular fa-envelope"></i></button>
-                <a href='www.linkedin.com/in/micah-francis-87bb0832' target='_blank' aria-label='Visit my LinkedIn page' rel='noopener noreferer'>LinkedIn<i className="fa-brands fa-linkedin"></i></a>
-                <a href='https://github.com/2ndborn' target='_blank' aria-label='Visit my GitHub page' rel="noopener noreferrer">GitHub<i className="fa-brands fa-github"></i></a>
+                <a
+                    href='https:/wa.me/447958774171?text=Hi Micah'
+                    target='_black'
+                    aria-label='Chat via WhatsApp'
+                    rel='noopener noreferrer'
+                >
+                    WhatsApp
+                    <i className="fa-brands fa-whatsapp"></i>
+                </a>
+                <a
+                    href='mailto: mfrancisr@hotmail.com?subject=Enquiry&body=Hi Micah'
+                    aria-label='Contact me via email'
+                >
+                    Email
+                    <i className="fa-regular fa-envelope"></i>
+                </a>
+                <a
+                    href='https://www.linkedin.com/in/micah-francis-87bb0832/'
+                    target='_blank'
+                    aria-label='Visit my LinkedIn page'
+                    rel='noopener noreferer'
+                >
+                    LinkedIn
+                    <i className="fa-brands fa-linkedin"></i>
+                </a>
+                <a 
+                href='https://github.com/2ndborn' 
+                target='_blank' 
+                aria-label='Visit my GitHub page' 
+                rel="noopener noreferrer"
+                >
+                    GitHub
+                    <i className="fa-brands fa-github"></i>
+                </a>
             </div>
             <div className={styles.lower}>
                 <div style={{ flex: 1, minWidth: '120px' }}>
@@ -33,11 +63,11 @@ const Footer = () => {
                 </div>
                 <div style={{ flex: 1, minWidth: '120px' }}>
                     <i className="fa-regular fa-envelope"></i>
-                    noemail@hotmail.com
+                    mfrancisr@hotmail.com
                 </div>
                 <div style={{ flex: 1, minWidth: '120px' }}>
                     <i className="fa-solid fa-mobile-screen"></i>
-                    07900 567 891
+                    07958 774 171
                 </div>
             </div>
         </motion.div>
