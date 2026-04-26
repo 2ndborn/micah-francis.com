cd49915292a805cae3836f86fee507c762c1fd73
Micah Francis <micah.francis69@googlemail.com>
2026-04-26 11:41:40 +0100

    added more code languages to the languages icons

--- micah/src/data/languageIcons.jsx
+++ micah/src/data/languageIcons.jsx
@@ -13,14 +13,20 @@ import { SiFlask } from "react-icons/si";
 import { SiDjango } from "react-icons/si";
 import { BiLogoPostgresql } from "react-icons/bi";
 import { SiMongodb } from "react-icons/si";
+import { SiReactbootstrap } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
+import { FaGitAlt } from "react-icons/fa";
 
 export const languageIcons = {
     HTML: FaHtml5,
     CSS: FaCss3Alt,
     JavaScript: FaJs,
     Python: FaPython,
+    GitHub: FaGithub,
+    Git: FaGitAlt,
     MaterializeCSS: DiMaterializecss,
     Bootstrap: FaBootstrap,
+    ReactBootstrap: SiReactbootstrap,
     Flask: SiFlask,
     Django: SiDjango,
     React: FaReact,
